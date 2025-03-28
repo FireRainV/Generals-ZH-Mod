2ce00ee0c4c537ce5b81918c53859c45bf31541a
FireRainV <tomerbd1234@gmail.com>
2025-03-28 12:45:42 +0300

    added custom maps to official maps

--- run.transfer_custom_maps.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "sourceFolder=ZH addon maps"
-set "destinationFolder=%userprofile%\Documents\Command and Conquer Generals Zero Hour Data\Maps"
-
-xcopy "%~dp0%sourceFolder%" "%destinationFolder%" /E /I /Y
-
-echo All files and folders have been copied.
-echo.
-pause
